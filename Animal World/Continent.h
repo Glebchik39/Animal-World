3e35f962ecfda00f96fb8682b8de097f45e6abad
Asus <Asus@DESKTOP-PH6TS4O>
2025-12-05 19:05:39 +0200

    сделал

---
+++ Animal World/Continent.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Carnivores.h"
+#include "Herbivore.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Continent
+{
+	vector<Herbivore> ptr;
+	vector<Carnivores>ptr1;
+public:
+	Continent() {};
+
+};
+
--- Animal World/Main.cpp
+++ Animal World/Main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main()
 {
+	
 	Animal* animal = nullptr;
 	int choice = 0;
 }
